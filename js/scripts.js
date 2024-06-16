7bdd64bfd0fb618ae581dc61d7757f44dbbbc74b
melaniemcc <melanie@hiddenlevers.com>
2024-06-16 18:17:58 -0400

    adding responsiveness and no results message

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,48 @@
-// getItems();
 let searchInput = document.querySelector('#search-input');
+let itemsContainer = document.querySelector('.items');
 
-document.querySelector('#search-btn').addEventListener('click', async function (event) {
-    // let category = filter ? filter : 'equipment';
-    // let url = `https://botw-compendium.herokuapp.com/api/v3/compendium/category/${category}`;
+document.querySelector('#search-form').addEventListener('submit', async function (event) {
+    event.preventDefault();
     let searchTerm = searchInput.value;
     let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`;
     const response = await fetch(url);
     const items = await response.json();
-    console.log(items);
-
-    displayItems(items);
+    if(items && items.meals) {
+        displayItems(items);
+    } else {
+        displayError();
+    }
 
 })
 
 function displayItems(items) {
-    let itemsContainer = document.querySelector('.items');
     itemsContainer.innerHTML = ``;
     for (let i = 0; i < 80; i++) {
         let data = items.meals[i];
         if(data && data.strMealThumb) {
         let item = document.createElement('div');
         item.className = 'item m-1';
+
+        //truncate long titles
+        let recipeTitle = data.strMeal.length > 38 ? data.strMeal.substring(0,38) + '...' : data.strMeal;
+
         item.innerHTML = `<div class="img-container">
-                            <img src="${data.strMealThumb}">
+                            <a href="${data.strSource}" target="_none"><img src="${data.strMealThumb}"></a>
                             <div class="dark-opacity"></div>
                           </div>
                           <div class="description">
                             <p class="category mb-1">${data.strCategory}</p>
                             <p class="origin mb-1">Origin: ${data.strArea}</p>
-                            <div class="title mb-3">${data.strMeal}</div>
+                            <div class="title mb-3">${recipeTitle }</div>
                             <a href="${data.strSource}" target="_none" class="my-2 btn read-more">Read Recipe</a>
-                          </div>
-                          <a class="hidden-link" target="_none" href="${data.strSource}"></a>  `;
+                          </div>`;
         itemsContainer.appendChild(item);
         }
         // console.log(items.data[i]);
     }
+}
+
+function displayError(){
+    itemsContainer.innerHTML = ``;
+    itemsContainer.innerHTML = `<p>Sorry, No Results Found. Try searching for something else!</p>`
 }
